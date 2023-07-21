4c105bf6ccbc1c1037890aa239370586e1b44d20
Gabriel Rocha <rochagbriel@gmail.com>
2023-07-21 20:55:44 +0200

    Added Navigation component

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
+
+  toMovies(): void {
+    this.router.navigate(['movies']);
+  }
+
+  toProfile(): void {
+    this.router.navigate(['profile']);
+  }
+
+  handleLogout(): void {
+    this.router.navigate(['welcome']);
+    localStorage.clear();
+  }
+
+}
